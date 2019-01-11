863b201c0f8938b629c6305c9e476e1b182efeb2
DavidTorpey <torpey.david93@gmail.com>
2019-01-11 17:43:32 +0000

    Add cropping

--- del.py
+++ del.py
@@ -1,3 +1,17 @@
+import numpy as np
+
+def get_cropped_videos(video):
+    _, r, c, _ = video.shape
+    
+    X = []
+    while len(X) < 5:
+        x = np.random.randint(0, c)
+        y = np.random.randint(0, r)
+        if y + 224 <= r and x + 224 <= c:
+            snippet = video[:, y:y+224, x:x+224, :]
+            X.append(snippet)
+    return np.array(X)
+
 
 classes = open('classes.txt').read().splitlines()
 
